c72662b2d1fdf6cf2d98b1460d4db8bd26cad303
Ivan <vandoslando@gmail.com>
2023-11-18 22:02:32 +0200

    schedule

---
+++ src/js/schedule.js
@@ -0,0 +1,32 @@
+const timeSchedule = [
+    {
+        text: "9:00am",
+        class: "leftBar"
+    },
+    {
+        text: "10:00am",
+        class: "leftBar"
+    },
+    {
+        text: "11:00am",
+        class: "leftBar"
+    },
+    {
+        text: "1:00pm",
+        class: "leftBar"
+    },
+    {
+        text: "4:00pm",
+        class: "leftBar"
+    },
+    {
+        text: "6:00pm",
+        class: "leftBar"
+    }
+]
+
+const daySchedule = [
+    {
+        text
+    }
+]
