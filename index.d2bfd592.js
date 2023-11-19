e85aa7df15bbe34c12333a779fe3a4251ee9106b
Voronyuk05 <85370955+Voronyuk05@users.noreply.github.com>
2023-11-19 19:43:33 +0000

    Deploying to gh-pages from @ Voronyuk05/gym_project@30818b517e0a91675dc18f460fe350295aed029b 🚀

---
+++ index.d2bfd592.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".burger"),t=document.querySelector(".navigation"),c=document.querySelector(".container");e.addEventListener("click",(function(o){document.body.classList.toggle("_lock"),e.classList.toggle("_active"),t.classList.toggle("_active"),c.classList.toggle("_menu-backgraund")}))}();
+//# sourceMappingURL=index.d2bfd592.js.map
